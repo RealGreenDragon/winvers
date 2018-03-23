8adab7fd906356a4a500e47e1256cae665751425
MagicGreenDragon <14246920+MagicGreenDragon@users.noreply.github.com>
2018-03-23 13:47:20 +0100

    v0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'winvers',
     packages = ['winvers'],
-    version = '0.1',
+    version = '0.1.1',
     description = 'Definitive Windows version checker for Python',
     long_description=open("README.rst").read(),
     author = 'Daniele Giudice',
